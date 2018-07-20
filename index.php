ed316c07e966b8ea3edf3ad9c36646164e111372
joeb15 <joebanko15@gmail.com>
2018-07-19 18:31:34 -0700

    index

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: SYSTEM
+ * Date: 7/19/2018
+ * Time: 6:30 PM
+ */
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+</body>
+</html>
